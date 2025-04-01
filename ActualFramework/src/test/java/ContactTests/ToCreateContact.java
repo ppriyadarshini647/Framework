e02f8343a9816bc7d3ae6def305ab3090b44fdb1
ppriyadarshini647 <prakrutipriyadarshini647@users.noreply.github.com>
2025-04-01 09:22:48 +0530

    pushing framework

---
+++ ActualFramework/src/test/java/ContactTests/ToCreateContact.java
@@ -0,0 +1,10 @@
+package ContactTests;
+
+public class ToCreateContact {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ActualFramework/src/test/java/OrganizationTests/ToCreateOrg.java
@@ -0,0 +1,10 @@
+package OrganizationTests;
+
+public class ToCreateOrg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
