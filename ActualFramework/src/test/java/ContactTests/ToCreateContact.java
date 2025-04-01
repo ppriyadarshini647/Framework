7afc61a84c8ae8d2cbbbbcfcef251b4728de92fc
souja <souja@Sowjanya>
2025-04-01 09:49:44 +0530

    Sending For Pull Request

--- ActualFramework/src/test/java/ContactTests/ToCreateContact.java
+++ ActualFramework/src/test/java/ContactTests/ToCreateContact.java
@@ -3,7 +3,7 @@ package ContactTests;
 public class ToCreateContact {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Contact created by Soujanya Successfully");
 
 	}
 
--- ActualFramework/src/test/java/OrganizationTests/ToCreateOrg.java
+++ ActualFramework/src/test/java/OrganizationTests/ToCreateOrg.java
@@ -3,7 +3,7 @@ package OrganizationTests;
 public class ToCreateOrg {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Org Created by Soujanya");
 
 	}
 
